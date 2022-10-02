527e95bc236408d97323d3ab5952888532d9bc01
Tim <tim.tkachenko@idealo.de>
2022-10-02 17:48:56 +0200

    clean up

--- jest.config.js
+++ jest.config.js
@@ -1,14 +1,3 @@
-//jest.config.js with ts-jest and jest-puppeteer
-// const { defaults: tsjPreset } = require('ts-jest/presets');
-
-// module.exports = {
-//   preset: 'jest-puppeteer',
-//   setupFilesAfterEnv: ['expect-puppeteer'],
-//   testTimeout: 100000,
-//   transform: {
-//     ...tsjPreset.transform,    
-//   }
-// }
 module.exports = {
   preset: './preset',
   testTimeout: 10000,
