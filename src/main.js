5c6e5d527115860b1b96ba189f5363ed68f13fa1
Tim <tim.tkachenko@idealo.de>
2022-10-26 14:43:58 +0200

    remove google adblock #1

--- src/main.js
+++ src/main.js
@@ -1,16 +1,6 @@
 import { removeCmp, observeForCMP, getCmpElements } from './dom.js';
 
-// TODO: make this dynamic
 const ignoreList = ['local.auspreiser.de'];
-
-function fixGoogle() {
-    const containerLayer = document.querySelectorAll('#container_layer, #tads');
-    if (!containerLayer) {
-        return;
-    }
-    containerLayer.forEach(e => e.remove());
-    removeSearchElementTopMargin();
-}
 
 function removeSearchElementTopMargin() {
     const searchElement = document.querySelector('#search');
@@ -23,8 +13,7 @@ function removeSearchElementTopMargin() {
 export function main() {
     if (ignoreList.includes(window.location.hostname)) {
         return;
-    }
-    fixGoogle();
+    }    
     const cmpElements = getCmpElements();
     if (cmpElements.length > 0) {
         removeCmp(cmpElements);
