2a28095789212a3d791e7401ef6b8b4f9a2cb560
Tim <tim.tkachenko@idealo.de>
2022-10-28 21:26:58 +0200

    add readme

--- readme.md
+++ readme.md
@@ -1,4 +1,7 @@
-# Mac firewall
+# FConsent
+FConsent is a simple browser extension that allows you to give automatic consent to cookies on multiple websites. No more clicking on "Accept All" or "Reject All" buttons.
+# Integration Tests
+## Mac firewall
 For avoiding mac firewall window, create a self signed certificate with parameters:
 - Identity Type: Self-Signed Root
 - Certificate Type: Code Signing
--- src/main.js
+++ src/main.js
@@ -2,14 +2,6 @@ import { removeCmp, observeForCMP, getCmpElements } from './dom.js';
 
 const ignoreList = ['local.auspreiser.de'];
 
-function removeSearchElementTopMargin() {
-    const searchElement = document.querySelector('#search');
-    if (!searchElement) {
-        return;
-    }
-    searchElement.style.marginTop = '0px';
-}
-
 export function main() {
     if (ignoreList.includes(window.location.hostname)) {
         return;
