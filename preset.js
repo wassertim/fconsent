693d0f5c6d61e64a7b101359aa1a7e7c2ec16605
Tim Tkachenko <wassertim@gmail.com>
2022-12-04 17:23:43 +0000

    remove puppeteer preset

--- preset.js
+++
@@ -1,8 +0,0 @@
-// preset.js
-const ts_preset = require('ts-jest/presets/js-with-babel/jest-preset');
-const puppeteer_preset = require('jest-puppeteer/jest-preset');
-
-module.exports = Object.assign(
-    puppeteer_preset,
-    ts_preset,
-);
